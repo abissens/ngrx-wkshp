0816f21f050c5043b0026afc183200238e9123ea
Abissens <hichem.abissens@gmail.com>
2024-02-11 18:45:16 +0100

    add filtering search on quotes

--- projects/raw-friends/src/app/friends/friends.module.ts
+++ projects/raw-friends/src/app/friends/friends.module.ts
@@ -5,6 +5,8 @@ import {EpisodeViewComponent} from './views/episode-view/episode-view.component'
 import {RouterLink} from '@angular/router';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatInputModule} from '@angular/material/input';
+import {ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
@@ -16,7 +18,9 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
     CommonModule,
     RouterLink,
     MatProgressBarModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatInputModule,
+    ReactiveFormsModule
   ]
 })
 export class FriendsModule { }
--- projects/raw-friends/src/app/friends/views/quotes-view/quotes-view.component.ts
+++ projects/raw-friends/src/app/friends/views/quotes-view/quotes-view.component.ts
@@ -1,9 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {BehaviorSubject, Observable} from 'rxjs';
+import {BehaviorSubject, debounceTime, distinctUntilChanged, map, mergeMap, Observable, startWith} from 'rxjs';
 import {Quote} from '../../domain/quote.model';
 import {QuoteService} from '../../services/quote.service';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import {RequestStatus} from '../data/request.data';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-quotes-view',
@@ -14,11 +15,22 @@ export class QuotesViewComponent implements OnInit {
 
   loadingQuotesStatus: RequestStatus = RequestStatus.LOADED;
 
+  searchQuoteControl = new FormControl('');
+
   private quotesSubject = new BehaviorSubject<Quote[]>([]);
-  public readonly quotes$: Observable<Quote[]> = this.quotesSubject.asObservable();
+  private readonly quotes$: Observable<Quote[]> = this.quotesSubject.asObservable();
+
+  public filteredQuotes$: Observable<Quote[]> =
+    this.searchQuoteControl.valueChanges.pipe(
+      startWith(this.searchQuoteControl.value), // make it emit first value
+      debounceTime(300),
+      distinctUntilChanged(),
+      mergeMap(searchValue => this.quotes$.pipe(map(quotes => quotes.filter(quote => this.searchFilter(quote, searchValue ?? '')))))
+    );
 
   constructor(private quoteService: QuoteService,
-              private snackBar: MatSnackBar) { }
+              private snackBar: MatSnackBar) {
+  }
 
   ngOnInit(): void {
     this.loadQuotes();
@@ -33,11 +45,15 @@ export class QuotesViewComponent implements OnInit {
       },
       error: err => {
         this.loadingQuotesStatus = RequestStatus.ERRORED;
-        this.snackBar.open(err.message , undefined, {
+        this.snackBar.open(err.message, undefined, {
           duration: 3000
         });
       },
     });
   }
 
+  private searchFilter(quote: Quote, searchQuery: string) {
+    return quote.character?.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      quote.text.toLowerCase().includes(searchQuery.toLowerCase())
+  }
 }
