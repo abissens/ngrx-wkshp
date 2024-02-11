7f8d18cdbe87653a85971c3fa962c1653dda5fcb
Abissens <hichem.abissens@gmail.com>
2024-02-11 18:11:43 +0100

    implement episode view

--- projects/raw-friends/src/app/friends/views/episode-view/episode-view.component.spec.ts
+++ projects/raw-friends/src/app/friends/views/episode-view/episode-view.component.spec.ts
@@ -1,21 +1,154 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { EpisodeViewComponent } from './episode-view.component';
+import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
+import {FriendsModule} from '../../friends.module';
+import {QuoteService} from '../../services/quote.service';
+import {Quote} from '../../domain/quote.model';
+import {Character} from '../../domain/character.model';
+import {Episode} from '../../domain/episode.model';
+import {of, Subject, throwError} from 'rxjs';
+import {AppRoutingModule} from '../../../app-routing.module';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {EpisodeViewComponent} from './episode-view.component';
+import {ActivatedRoute} from '@angular/router';
 
 describe('EpisodeViewComponent', () => {
-  let component: EpisodeViewComponent;
-  let fixture: ComponentFixture<EpisodeViewComponent>;
+  let quoteService: jest.Mocked<QuoteService>;
 
+  // init quotes mock service
+  beforeEach(() => {
+    quoteService = quoteService = {
+      getQuotes: jest.fn(),
+      getCharacters: jest.fn(),
+      getEpisodes: jest.fn(),
+    } as unknown as jest.Mocked<QuoteService>;
+  });
+
+  // init test bed
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [EpisodeViewComponent]
+      declarations: [EpisodeViewComponent],
+      imports: [FriendsModule, AppRoutingModule, NoopAnimationsModule],
+      // js-dom cannot handle animations
+      // so import NoopAnimationsModule
+      providers: [
+        {provide: QuoteService, useValue: quoteService},
+        {
+          provide: ActivatedRoute,
+          useValue: {params: of({id: '1'})} // Provide a mock ActivatedRoute with a test parameter
+        }
+      ],
     });
-    fixture = TestBed.createComponent(EpisodeViewComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('create', () => {
+    const fixture = TestBed.createComponent(EpisodeViewComponent);
+    expect(fixture.componentInstance).toBeTruthy();
   });
+
+  it('load quotes on initialization', () => {
+    // get quotes init
+    quoteService.getQuotes.mockReturnValue(of(mockQuotes));
+
+    // prepare fixture
+    const fixture = TestBed.createComponent(EpisodeViewComponent);
+    fixture.detectChanges();
+
+    // load mock quotes
+    expect(selectFixture(fixture, '.quote-item:nth-child(1) .character-name').textContent).toContain('Character 1');
+    expect(selectFixture(fixture, '.quote-item:nth-child(1) .quote-text').textContent).toContain('Quote 1');
+    expect(selectAllFixture(fixture, '.quote-item')).toHaveLength(1); // only episode 1 quotes
+  });
+
+  it('display "Loading quotes..." on loading', () => {
+    // Given empty observable
+    let subject = new Subject<Quote[]>();
+    quoteService.getQuotes.mockReturnValue(subject);
+
+    // prepare fixture
+    const fixture = TestBed.createComponent(EpisodeViewComponent);
+    fixture.detectChanges();
+
+    expect(selectFixture(fixture, '.loading-indicator').textContent).toContain('Loading quotes...');
+
+    // load mock quotes
+    subject.next(mockQuotes);
+    subject.complete();
+    fixture.detectChanges();
+
+    expect(selectFixture(fixture, '.loading-indicator')).toBeNull();
+
+    expect(selectFixture(fixture, '.quote-item:nth-child(1) .character-name').textContent).toContain('Character 1');
+    expect(selectFixture(fixture, '.quote-item:nth-child(1) .quote-text').textContent).toContain('Quote 1');
+    expect(selectAllFixture(fixture, '.quote-item')).toHaveLength(1); // only episode 1 quotes
+
+  });
+
+  it('display "No quotes available." on empty', () => {
+    // get quotes init
+    quoteService.getQuotes.mockReturnValue(of([]));
+
+    // prepare fixture
+    const fixture = TestBed.createComponent(EpisodeViewComponent);
+    fixture.detectChanges();
+
+    // no quotes
+    expect(selectFixture(fixture, '.no-quotes').textContent).toContain('No quotes available.');
+  });
+
+  it('display "Cannot load quotes." on error', () => {
+    // get quotes is errored
+    quoteService.getQuotes.mockReturnValue(throwError(() => new Error("some error")));
+
+    // prepare fixture
+    const fixture = TestBed.createComponent(EpisodeViewComponent);
+    fixture.detectChanges();
+
+    // no quotes
+    expect(selectFixture(fixture, '.err-quotes').textContent).toContain('Cannot load quotes.');
+  });
+
+  it('fire snackbar on error for 3 seconds', fakeAsync(() => {
+    // get quotes is errored
+    quoteService.getQuotes.mockReturnValue(throwError(() => new Error("some error")));
+
+    // prepare fixture
+    const fixture = TestBed.createComponent(EpisodeViewComponent);
+    fixture.detectChanges();
+
+    // err snack bar
+    expect(selectFixtureParent(fixture, '.mat-mdc-snack-bar-label').textContent).toContain('some error');
+
+    // wait 2 seconds
+    tick(2500);
+    expect(selectFixtureParent(fixture, '.mat-mdc-snack-bar-label').textContent).toContain('some error');
+
+    // wait extra 1 seconds
+    tick(1000);
+    expect(selectFixtureParent(fixture, '.mat-mdc-snack-bar-label')).toBeNull();
+  }));
 });
+
+const mockCharacters: Character[] = [
+  {id: 1, name: 'Character 1'},
+  {id: 2, name: 'Character 2'},
+];
+const mockEpisodes: Episode[] = [
+  {id: 1, title: 'title 1', season: 1},
+  {id: 2, title: 'title 2', season: 2},
+];
+
+const mockQuotes: Quote[] = [
+  {character: mockCharacters[0], text: 'Quote 1', episode: mockEpisodes[0]},
+  {character: mockCharacters[1], text: 'Quote 2', episode: mockEpisodes[1]},
+];
+
+function selectFixture<T>(fixture: ComponentFixture<T>, selector: string) {
+  return fixture.nativeElement.querySelector(selector);
+}
+
+function selectAllFixture<T>(fixture: ComponentFixture<T>, selector: string) {
+  return fixture.nativeElement.querySelectorAll(selector);
+}
+
+function selectFixtureParent<T>(fixture: ComponentFixture<T>, selector: string) {
+  return fixture.nativeElement.parentNode.querySelector(selector);
+}
--- projects/raw-friends/src/app/friends/views/episode-view/episode-view.component.ts
+++ projects/raw-friends/src/app/friends/views/episode-view/episode-view.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RequestStatus} from '../data/request.data';
+import {BehaviorSubject, map, Observable} from 'rxjs';
+import {Quote} from '../../domain/quote.model';
+import {QuoteService} from '../../services/quote.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-episode-view',
   templateUrl: './episode-view.component.html',
   styleUrls: ['./episode-view.component.less']
 })
-export class EpisodeViewComponent {
+export class EpisodeViewComponent implements OnInit {
+  loadingQuotesStatus: RequestStatus = RequestStatus.LOADED;
 
+  private quotesSubject = new BehaviorSubject<Quote[]>([]);
+  public readonly quotes$: Observable<Quote[]> = this.quotesSubject.asObservable();
+
+  constructor(private quoteService: QuoteService,
+              private route: ActivatedRoute,
+              private snackBar: MatSnackBar) {
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      const episodeId = +params['id'];
+      this.loadQuotesForEpisode(episodeId);
+    });
+  }
+
+  loadQuotesForEpisode(episodeId: number): void {
+    this.loadingQuotesStatus = RequestStatus.LOADING;
+    this.quoteService.getQuotes()
+      .pipe(map(quotes => quotes.filter(q => q.episode.id === episodeId)))
+      .subscribe({
+        next: quotes => {
+          this.loadingQuotesStatus = RequestStatus.LOADED;
+          this.quotesSubject.next(quotes);
+        },
+        error: err => {
+          this.loadingQuotesStatus = RequestStatus.ERRORED;
+          this.snackBar.open(err.message, undefined, {
+            duration: 3000
+          });
+        },
+      });
+  }
 }
